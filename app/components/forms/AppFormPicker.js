46e232b9d0447924ab5ced1ccc0d3a2fc598c361
jtodd3 <jtodd@vendasta.com>
2021-03-22 00:49:13 -0600

    add dynamic input width

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,11 +4,12 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder }) {
+function AppFormPicker({ items, name, placeholder, width }) {
 	const { errors, setFieldValue, touched, values } = useFormikContext();
 	return (
 		<>
 			<AppPicker
+				width={width}
 				items={items}
 				onSelectItem={(item) => setFieldValue(name, item)}
 				placeholder={placeholder}
