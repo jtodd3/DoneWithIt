05861ae2f08eda364d056aac54adc569fc13e24a
jtodd3 <jtodd@vendasta.com>
2021-03-27 21:52:18 -0600

    chore: request camera permission v1

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import * as ImagePicker from "expo-image-picker";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingEditScreen />;
+	const requestPermissions = async () => {
+		const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+		if (!granted) {
+			alert("You need to enable permission to access the library");
+		}
+	};
+
+	useEffect(() => {
+		requestPermissions();
+	}, []);
+
+	return <Screen></Screen>;
 }
