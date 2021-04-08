2a953be03b81ffe80bca990f2b5bba680a770027
jtodd3 <jtodd@vendasta.com>
2021-04-08 14:17:47 -0600

    chore: add auth navigator

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -15,8 +15,12 @@ function WelcomeScreen(props) {
 				<Text style={styles.tagline}>Sell What You Don't Need</Text>
 			</View>
 			<View style={styles.buttonsContainer}>
-				<Button title="Login" />
-				<Button title="Register" color="secondary" />
+				<Button title="Login" onPress={() => navigation.navigate("Login")} />
+				<Button
+					title="Register"
+					color="secondary"
+					onPress={() => navigation.navigate("Register")}
+				/>
 			</View>
 		</ImageBackground>
 	);
