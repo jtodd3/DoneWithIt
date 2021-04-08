5d7ef858b97f59ee8578c2ea694838e5886ceb2c
jtodd3 <jtodd@vendasta.com>
2021-04-07 21:58:59 -0600

    chore: introduce basic naviagtion to app.js

--- App.js
+++ App.js
@@ -1,6 +1,43 @@
 import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
+import Screen from "./app/components/Screen";
+
+const Link = () => {
+	const navigation = useNavigation();
+
+	return (
+		<Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+	);
+};
+
+const Tweets = ({ navigation }) => (
+	<Screen>
+		<Text>Tweets</Text>
+		<Link />
+	</Screen>
+);
+
+const TweetDetails = () => (
+	<Screen>
+		<Text>Tweet Details</Text>
+	</Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+	<Stack.Navigator>
+		<Stack.Screen name="Tweets" component={Tweets}></Stack.Screen>
+		<Stack.Screen name="TweetsDetails" component={TweetDetails}></Stack.Screen>
+	</Stack.Navigator>
+);
 
 export default function App() {
-	return <ListingEditScreen />;
+	return (
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
+	);
 }
