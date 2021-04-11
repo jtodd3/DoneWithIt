31fb45ee1e27ef82655f3c2f59c9bce84c8e7ac5
jtodd3 <jtodd@vendasta.com>
2021-04-11 01:14:26 -0600

    chore: add useApi hook

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -11,38 +11,23 @@ import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 
 function ListingsScreen({ navigation }) {
-	const [listings, setListings] = useState([]);
-	const [error, setError] = useState(false);
-	const [loading, setLoading] = useState(false);
+	const getListingsApi = useApi(listingsApi.getListings);
 
 	useEffect(() => {
-		loadListings();
+		getListingsApi.request();
 	}, []);
-
-	const loadListings = async () => {
-		setLoading(true);
-		const response = await listingsApi.getListings();
-		setLoading(false);
-
-		if (!response.ok) {
-			return setError(true);
-		}
-
-		setError(false);
-		setListings(response.data);
-	};
 
 	return (
 		<Screen style={styles.screen}>
-			{error && (
+			{getListingsApi.error && (
 				<>
 					<AppText>We couldn't retrieve the listings.</AppText>
 					<Button title="Retry" onPress={loadListings} />
 				</>
 			)}
-			<ActivityIndicator visible={loading} />
+			<ActivityIndicator visible={getListingsApi.loading} />
 			<FlatList
-				data={listings}
+				data={getListingsApi.data}
 				keyExtractor={(listing) => listing.id.toString()}
 				renderItem={({ item }) => (
 					<Card
