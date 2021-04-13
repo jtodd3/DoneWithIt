aae456f71ba834b455a372ebc3b49077691e2217
jtodd3 <jtodd@vendasta.com>
2021-04-12 22:46:02 -0600

    feat: persist auth state when restarting

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React, { userState } from "react";
+import React, { userState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import * as navigationTheme from "./app/navigation/navigationTheme";
+import jwtDecode from "jwt-decode";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
 
 export default function App() {
 	const [user, setUser] = userState();
+
+	const restoreToken = async () => {
+		const token = await authStorage.getToken();
+		if (!token) {
+			return;
+		}
+		setUser(jwtDecode(token));
+	};
+
+	useEffect(() => {
+		restoreToken();
+	}, []);
 
 	return (
 		<AuthContext.Provider value={{ user, setUser }}>
