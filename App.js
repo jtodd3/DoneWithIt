924e8baa4098fca6539f664be10cae02a3d8ed28
jtodd3 <jtodd@vendasta.com>
2021-04-12 22:50:14 -0600

    feat: show splash screen while user is fetched

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { userState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import * as navigationTheme from "./app/navigation/navigationTheme";
 import jwtDecode from "jwt-decode";
+import { AppLoading } from "expo";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
@@ -11,6 +12,7 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
 	const [user, setUser] = userState();
+	const [isReady, setIsReady] = useState(false);
 
 	const restoreToken = async () => {
 		const token = await authStorage.getToken();
@@ -20,9 +22,11 @@ export default function App() {
 		setUser(jwtDecode(token));
 	};
 
-	useEffect(() => {
-		restoreToken();
-	}, []);
+	if (!isReady) {
+		return (
+			<AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+		);
+	}
 
 	return (
 		<AuthContext.Provider value={{ user, setUser }}>
