61eff421643244530a8f8e49c192006dc47bc04e
jtodd3 <jtodd@vendasta.com>
2021-04-12 22:19:17 -0600

    feat: store current user

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { userState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import * as navigationTheme from "./app/navigation/navigationTheme";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
+	const [user, setUser] = userState();
+
 	return (
-		<>
+		<AuthContext.Provider value={{ user, setUser }}>
 			<OfflineNotice />
 			<NavigationContainer theme={navigationTheme}>
-				<AuthNavigator />
+				{user ? <AppNavigator /> : <AuthNavigator />}
 			</NavigationContainer>
-		</>
+		</AuthContext.Provider>
 	);
 }
