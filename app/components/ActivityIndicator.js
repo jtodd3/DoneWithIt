0ba8a5655f00a2c0871350c2574e5db057d29171
jtodd3 <jtodd@vendasta.com>
2021-04-13 00:23:51 -0600

    feat: turning ActivityIndicator into an overlay

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import LottieView from "lottie-react-native";
+
+import colors from "../config/colors";
 
 function ActivityIndicator({ visible = false }) {
 	if (!visible) {
@@ -7,12 +10,25 @@ function ActivityIndicator({ visible = false }) {
 	}
 
 	return (
-		<LottieView
-			autoPlay
-			loop
-			source={require("../assets/animations/loader.json")}
-		/>
+		<View style={styles.overlay}>
+			<LottieView
+				autoPlay
+				loop
+				source={require("../assets/animations/loader.json")}
+			/>
+		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	overlay: {
+		position: "absolute",
+		backgroundColor: colors.white,
+		height: "100%",
+		opacity: 0.8,
+		width: "100%",
+		zIndex: 1,
+	},
+});
 
 export default ActivityIndicator;
