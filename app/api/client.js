9b86c2a7095e904a9e21599c03c56c78969f9cda
jtodd3 <jtodd@vendasta.com>
2021-04-13 23:03:50 -0600

    feat: add dev/staging/prod url settings

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
 import authStorage from "../auth/storage";
+import settings from "../config/settings";
 
 const apiClient = requirecreate({
-	baseURL: "http://142.165.42.56:9000/api",
+	baseURL: settings.apiUrl,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
